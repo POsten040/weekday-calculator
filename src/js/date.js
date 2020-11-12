5ed84f4bfac4da27691e89d9fa7af963a4ed447d
Austin Schrader <austin.jschrader@gmail.com>
2020-11-12 11:14:12 -0800

    Add a function to take in a number and return the day of the week
    <co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- src/js/date.js
+++ src/js/date.js
@@ -1,1 +1,14 @@
-// https://www.w3schools.com/jsref/jsref_getday.asp
+export const checkDay = (num) => {
+	let daysOfWeek = new Map([
+		[0, 'Monday'],
+		[1, 'Tuesday'],
+		[2, 'Wednesday'],
+		[3, 'Thursday'],
+		[4, 'Friday'],
+		[5, 'Saturday'],
+		[6, 'Sunday'],
+	]);
+
+	let selectedDay = daysOfWeek.get(num);
+	return selectedDay;
+};
