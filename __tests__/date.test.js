749bf660b92a750bad95a687c47bbae0eae8cf83
Austin Schrader <austin.jschrader@gmail.com>
2020-11-12 11:49:21 -0800

    Add in the test condition
    <co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- __tests__/date.test.js
+++ __tests__/date.test.js
@@ -1,1 +1,9 @@
-import { checkValue } from './../src/function.js';
+import { checkDay } from './../src/js/date.js';
+
+describe('checkDay', () => {
+	test('should take an inputted date and return the day of the week that corresponds to it.', () => {
+		let date = 4;
+		let dayOfWeek = checkDay(date);
+		expect(dayOfWeek).toEqual('Friday');
+	});
+});
