d622e8b3203c6677a26a693bbfc9a92c7bbb0c30
Austin Schrader <austin.jschrader@gmail.com>
2020-11-12 10:20:07 -0800

    Added UI logic for the date form
    <co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 // import Object from './js/object.js';
 // import Post from './js/post.js';
+
+$(document).ready(function () {
+	$('#weekday-check-form').submit(function (event) {
+		event.preventDefault();
+		var date = new Date($('#user-date').val());
+		console.log(date);
+		let day = date.getDate();
+		let month = date.getMonth() + 1;
+		let year = date.getFullYear();
+		console.log([month, day, year].join('/'));
+	});
+});
