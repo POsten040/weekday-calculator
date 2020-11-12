1aa60b3d99f3fd66f1897eea88ed14258e7a89f1
Austin Schrader <austin.jschrader@gmail.com>
2020-11-12 11:14:50 -0800

    Removed code that we were not using, changed the import and called the checkDay function
    <co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,17 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { checkDay } from './js/date.js';
 // import Object from './js/object.js';
 // import Post from './js/post.js';
 
 $(document).ready(function () {
 	$('#weekday-check-form').submit(function (event) {
 		event.preventDefault();
-		var date = new Date($('#user-date').val());
-		console.log(date);
-		let day = date.getDate();
-		let month = date.getMonth() + 1;
-		let year = date.getFullYear();
-		console.log([month, day, year].join('/'));
+		let date = new Date($('#user-date').val());
+		let dayOfWeek = date.getDay();
+		checkDay(dayOfWeek);
 	});
 });
